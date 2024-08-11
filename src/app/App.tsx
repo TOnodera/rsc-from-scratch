07f5760869e2b3ac8cb7664d29ed8ef6bd1fa3dc
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-08-11 10:39:44 +0000

    クライアントコンポーネントの分割まで

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,17 @@
+import React from 'react';
 import { Page } from './Page.js';
+
+const Clock = {
+  $$typeof: Symbol.for('react.module.reference'),
+  filepath: 'src/app/Clock.tsx',
+  name: 'Clock',
+} as unknown as React.ComponentType;
+
 export const App: React.FC = () => {
   return (
     <Page>
       <p>Hello world</p>
+      <Clock />
     </Page>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,11 @@ import rsdws from 'react-server-dom-webpack/server';
 const { renderToPipeableStream } = rsdws;
 import { App } from './app/App.js';
 
-renderToPipeableStream(<App />).pipe(process.stdout);
+const bundlerConfig = {
+  'src/app/Clock.tsx': {
+    Clock: {
+      pika: 'chu',
+    },
+  },
+};
+renderToPipeableStream(<App />, bundlerConfig).pipe(process.stdout);
