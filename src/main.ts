9871e1a60a190b76cd196c413fd8e47ad11ab092
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-08-11 03:12:03 +0000

    react-server-dom-webpackの依存が解決できない。。

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { build as esbuild } from 'esbuild';
 import { resolveApp, resolveBuild } from './utils';
 import { renderToString } from 'react-dom/server';
 import { createElement } from 'react';
+import * as ReactServerDom from 'react-server-dom-webpack/server';
 
 const app = express();
 
 app.get('/', async (req: Request, res: Response) => {
   const Page = await import('./build/index.js');
+  const stream = ReactServerDom.
   const html = renderToString(createElement(Page.default));
   res.send(html);
 });
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,30 @@
-import React from "react";
-export default function Page(){
-    return <h1>Hello server component!</h1>
+import React, { Suspense } from 'react';
+import { getAll } from '../data/db';
+
+export default function Page() {
+  return (
+    <>
+      <h1>Hello server component!</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Albums />
+      </Suspense>
+    </>
+  );
+}
+
+async function Albums() {
+  const albums = await getAll();
+  return (
+    <ul>
+      {albums.map((album) => (
+        <li key={album.id}>
+          <img src={album.cover} alt={album.title} />
+          <div>
+            <h3>{album.title}</h3>
+            <p>{album.songs.length}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
