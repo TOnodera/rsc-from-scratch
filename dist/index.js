3b580e23d2a2c038c32a020acd68ebb16ab03db8
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-08-11 09:11:22 +0000

    ベース作成まで

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+// @ts-expect-error
+import rsdws from 'react-server-dom-webpack/server';
+const { renderToPipeableStream } = rsdws;
+import { App } from './app/App.js';
+renderToPipeableStream(_jsx(App, {})).pipe(process.stdout);
---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>React Server Component Example</h1>
+      <p>Hello world</p>
+    </div>
+  );
+};
