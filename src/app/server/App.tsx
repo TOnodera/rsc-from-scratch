84a88933218813b4a91ea66d5be87fb1d987d229
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-08-14 05:10:23 +0000

    ssrでasync/await

--- src/app/server/App.tsx
+++ src/app/server/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Page } from '../Page.js';
 import { Client } from './Client.js';
+import { Uhyo } from './Uhyo.js';
 
 export const App: React.FC = () => {
   return (
     <Page>
       <p>Hello world</p>
+      <Uhyo />
       <Client.Clock />
     </Page>
   );
---
+++ src/app/server/Uhyo.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import type {} from 'react/experimental';
+
+export const Uhyo: React.FC = async () => {
+  const uhyoData = (
+    await import('../../data/uhyo.json', {
+      with: { type: 'json' },
+    })
+  ).default;
+  return (
+    <section>
+      <h2>Hello, I'm {uhyoData.name}</h2>
+      <p>I am {uhyoData.age}</p>
+      <p>My favorite languages are:</p>
+      <ul>
+        {uhyoData.favoriteLanguages.map((lang, i) => (
+          <li key={i}>{lang}</li>
+        ))}
+      </ul>
+    </section>
+  );
+};
